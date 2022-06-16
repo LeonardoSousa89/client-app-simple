00626ac41e696bcf276576dcfa179a5296132f18
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-06-16 01:49:36 -0300

    iterando sobre array do servidor 2

--- public/main/index.js
+++ public/main/index.js
@@ -17,8 +17,8 @@ function loadData(){
     fetch(URL,config).then(response=>response.json())
             .then(response=>{
                 for(let i=0;i<response.length;i++){
-                        console.log(variable.id.textContent=response[i].id)
-                        console.log(variable.msg.textContent=response[i].msg)
+                        variable.id.append(response[i].id)
+                        variable.msg.append(response[i].msg)
                 }
             })
             .catch(e=>console.log(e))
