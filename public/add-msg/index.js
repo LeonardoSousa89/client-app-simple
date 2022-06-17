44f9c1d851c4572ac6b21b8d4f91b577869b9956
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-06-16 22:06:05 -0300

    finalização do modulo add-msg

--- public/add-msg/index.js
+++ public/add-msg/index.js
@@ -1,29 +1,26 @@
 const doc=document
 
-const message=doc.getElementById('msg')
+let msg=doc.getElementById('msg')
 const enviar=doc.getElementById('enviar')
 
 enviar.addEventListener('click',()=>{
-    let msg=message.value
     ajax(msg)
-    clear()
 })
-
-function clear(){
-    message.value=''
-}
 
 async function ajax(msg){
     const URL='https://server-app-simple.herokuapp.com/add-msg'
+    let data={msg:msg.value}
     const config={method:'POST', 
-                  body :JSON.stringify(msg),
-                  headers :{
-                  'Content-Type': 'application/json'
+                  body:JSON.stringify(data),
+                  headers:{
+                  'Content-Type':'application/json'
                 }}
-    fetch(URL,config)
+    await fetch(URL,config)
                 .then(response=>{
                     if(response.ok){
+                       response.json()
                        console.log('requisicao bem sucedida!')
+                       doc.location.href='https://client-app-simple.herokuapp.com/'
                     }else{
                         console.log('houve um erro ao processar sua requisição!')
                         console.log(response.statusText)
