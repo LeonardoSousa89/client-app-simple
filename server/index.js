df79984ac322a098cdb6de2a284ea442a3dfad3a
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-06-17 11:51:40 -0300

    reformulacao do app (remocap dos modulos update e delete)

--- server/index.js
+++ server/index.js
@@ -7,11 +7,5 @@ router.route('/').get((req, res)=>{
 router.route('/add-msg').get((req, res)=>{
     res.sendFile(__dirname + '/views/add-msg.html')
 })
-router.route('/upt-msg/:id').get((req, res)=>{
-    res.sendFile(__dirname + '/views/upt-msg.html')
-})
-router.route('/rmv-msg/:id').get((req, res)=>{
-    res.sendFile(__dirname + '/views/rmv-msg.html')
-})
 
 module.exports = router
